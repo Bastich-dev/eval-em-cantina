fcb8d0efafe940a998b8353dcaa6911484fdf818
unknown <bastien.chantrel.pro@gmail.com>
2023-10-04 17:24:43 +0200

    last fix for vercel 2

--- api/index.js
+++ api/index.js
@@ -7,20 +7,7 @@ const pathData = "../data.json";
 const app = express();
 
 var allowedOrigins = ["http://localhost:3000", "https://cantina-starwars.vercel.app/"];
-app.use(
-    cors({
-        origin: function (origin, callback) {
-            // allow requests with no origin
-            // (like mobile apps or curl requests)
-            if (!origin) return callback(null, true);
-            if (allowedOrigins.indexOf(origin) === -1) {
-                var msg = "The CORS policy for this site does not " + "allow access from the specified Origin.";
-                return callback(new Error(msg), false);
-            }
-            return callback(null, true);
-        },
-    })
-);
+app.use(cors({}));
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "../build")));
 
